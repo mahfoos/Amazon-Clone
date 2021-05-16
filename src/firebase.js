c0b68ade0699902a5d5aab0c6a42eb6b73bcc9a5
Mahfooz <umahfoosahamed@gmail.com>
2021-05-16 14:13:14 +0530

    cleaned files

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAI_5EEp6ba8r3lqZs48tsyFdtP04cC6uU",
+    authDomain: "app-cb5a6.firebaseapp.com",
+    projectId: "app-cb5a6",
+    storageBucket: "app-cb5a6.appspot.com",
+    messagingSenderId: "50806429849",
+    appId: "1:50806429849:web:a1fb6603d65fe9173f4008"
+  };
