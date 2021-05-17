181b33367460e27c082cbd022d0b8389f6002901
Mahfooz <umahfoosahamed@gmail.com>
2021-05-17 15:00:09 +0530

    commit

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Checkout.css";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
   return (
@@ -13,7 +14,7 @@ function Checkout() {
         </div>
       </div>
       <div className="checkout_right">
-          <Subtotal/>
+         <Subtotal/>
         
       </div>
     </div>
